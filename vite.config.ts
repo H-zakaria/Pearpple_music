73394121e594589eaaf64cc3e1842ce9e59b39af
H-zakaria <hamdi.zakaria@protonmail.com>
2023-11-22 20:10:35 +0100

    ajout breakpoints scss + vite auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+const pathSrc = path.resolve(__dirname, "./src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "./src/styles/breakpoints.scss";` },
+    },
+  },
+});
